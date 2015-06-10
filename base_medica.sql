38c6c8d7d7a29504d2166a463cb255a15f74ed9c
Julio Vega <307333180@ciencias.unam.mx>
2015-06-10 18:05:26 -0500

    actualización tabla cita 'tipo m_cedula'

--- base_medica.sql
+++ base_medica.sql
@@ -1,5 +1,5 @@
 create database if not exists citas_medicas;
-
+use citas_medicas;
 create table medico(
 m_cedula varchar(30) primary key,
 m_nombre varchar(255),
@@ -16,14 +16,21 @@ p_telefono varchar(30)
 );
 
 create table cita(
-m_cedula int primary key,
+m_cedula varchar(30) primary key,
 p_num_paciente int,
 c_fecha date,
 foreign key (m_cedula) references medico(m_cedula),
 foreign key (p_num_paciente) references paciente(p_num_paciente)
 );
 
-create table usuarios(
-usuario varchar(15),
+use citas_medicas;
+drop table usuarios;
+create table usuario(
+usuario varchar(15) primary key,
 contraseña varchar(40) -- firma sha1
+
 );
+
+insert into usuario values ('medico1',sha1('medico1')),('medico2',sha1('medico2'));
+
+select * from usuario;
