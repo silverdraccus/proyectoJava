db23cc705919aa8094e5f8909a0f6793857cc8ca
silverdraccus <silver.draccus@gmail.com>
2015-06-11 08:53:52 -0500

    adminDao

--- Codigo/citas_medicas/src/main/java/mx/unam/citas_medicas/dao/impl/AdministradorDAOImpl.java
+++ Codigo/citas_medicas/src/main/java/mx/unam/citas_medicas/dao/impl/AdministradorDAOImpl.java
@@ -1,28 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.unam.citas_medicas.dao.impl;
 
+import java.util.List;
 import mx.unam.citas_medicas.dao.*;
+import mx.unam.citas_medicas.modelo.Administrador;
+import org.hibernate.LockOptions;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Ariadna
  */
-public interface AdministradorDAOImpl {
-         private static final Logger log = LoggerFactory.getLogger(AdministradorDAOImpl.class);
-		//property constants
-	public static final String NOMBRE = "nombre";
+public class AdministradorDAOImpl implements AdministradorDAO{
+    private static final Logger log = LoggerFactory.getLogger(AdministradorDAOImpl.class);
+    private SessionFactory sessionFactory;
+    public static final String NOMBRE = "nombre";
 
+    public void setSessionFactory(SessionFactory sf){
+        this.sessionFactory = sf;
+    }
 
-
-    
+    @Override
     public void save(Administrador transientInstance) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("saving Administrador instance");
         try {
-            getSession().save(transientInstance);
+            session.save(transientInstance);
             log.debug("save successful");
         } catch (RuntimeException re) {
             log.error("save failed", re);
@@ -30,10 +37,12 @@ public interface AdministradorDAOImpl {
         }
     }
     
+    @Override
 	public void delete(Administrador persistentInstance) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("deleting Administrador instance");
         try {
-            getSession().delete(persistentInstance);
+            session.delete(persistentInstance);
             log.debug("delete successful");
         } catch (RuntimeException re) {
             log.error("delete failed", re);
@@ -41,10 +50,12 @@ public interface AdministradorDAOImpl {
         }
     }
     
+    @Override
     public Administrador findById( java.lang.String id) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("getting Administrador instance with id: " + id);
         try {
-            Administrador instance = (Administrador) getSession()
+            Administrador instance = (Administrador) session
                     .get("Administrador", id);
             return instance;
         } catch (RuntimeException re) {
@@ -54,10 +65,12 @@ public interface AdministradorDAOImpl {
     }
     
     
+    @Override
     public List findByExample(Administrador instance) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("finding Administrador instance by example");
         try {
-            List results = getSession()
+            List results = session
                     .createCriteria("Administrador")
                     .add(Example.create(instance))
             .list();
@@ -69,44 +82,50 @@ public interface AdministradorDAOImpl {
         }
     }    
     
+    @Override
     public List findByProperty(String propertyName, Object value) {
-      log.debug("finding Administrador instance with property: " + propertyName
-            + ", value: " + value);
-      try {
-         String queryString = "from Administrador as model where model." 
-         						+ propertyName + "= ?";
-         Query queryObject = getSession().createQuery(queryString);
-		 queryObject.setParameter(0, value);
-		 return queryObject.list();
-      } catch (RuntimeException re) {
-         log.error("find by property name failed", re);
-         throw re;
-      }
-	}
+        Session session = this.sessionFactory.getCurrentSession();
+        log.debug("finding Administrador instance with property: " + propertyName
+              + ", value: " + value);
+        try {
+            String queryString = "from Administrador as model where model." 
+                                                          + propertyName + "= ?";
+            Query queryObject = session.createQuery(queryString);
+                   queryObject.setParameter(0, value);
+                   return queryObject.list();
+        } catch (RuntimeException re) {
+           log.error("find by property name failed", re);
+           throw re;
+        }
+    }
 
-	public List findByNombre(Object nombre
-	) {
-		return findByProperty(NOMBRE, nombre
-		);
-	}
+    @Override
+    public List findByNombre(Object nombre) {
+        Session session = this.sessionFactory.getCurrentSession();
+            return findByProperty(NOMBRE, nombre
+            );
+    }
 	
-
-	public List findAll() {
-		log.debug("finding all Administrador instances");
-		try {
-			String queryString = "from Administrador";
-	         Query queryObject = getSession().createQuery(queryString);
-			 return queryObject.list();
-		} catch (RuntimeException re) {
-			log.error("find all failed", re);
-			throw re;
-		}
-	}
+    @Override
+    public List findAll() {
+        Session session = this.sessionFactory.getCurrentSession();
+        log.debug("finding all Administrador instances");
+        try {
+                String queryString = "from Administrador";
+         Query queryObject = session.createQuery(queryString);
+                 return queryObject.list();
+        } catch (RuntimeException re) {
+                log.error("find all failed", re);
+                throw re;
+        }
+    }
 	
+    @Override
     public Administrador merge(Administrador detachedInstance) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("merging Administrador instance");
         try {
-            Administrador result = (Administrador) getSession()
+            Administrador result = (Administrador) session
                     .merge(detachedInstance);
             log.debug("merge successful");
             return result;
@@ -116,10 +135,12 @@ public interface AdministradorDAOImpl {
         }
     }
 
+    @Override
     public void attachDirty(Administrador instance) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("attaching dirty Administrador instance");
         try {
-            getSession().saveOrUpdate(instance);
+            session.saveOrUpdate(instance);
             log.debug("attach successful");
         } catch (RuntimeException re) {
             log.error("attach failed", re);
@@ -127,11 +148,13 @@ public interface AdministradorDAOImpl {
         }
     }
     
+    @Override
     public void attachClean(Administrador instance) {
+        Session session = this.sessionFactory.getCurrentSession();
         log.debug("attaching clean Administrador instance");
         try {
-                      	getSession().buildLockRequest(LockOptions.NONE).lock(instance);
-          	            log.debug("attach successful");
+            session.buildLockRequest(LockOptions.NONE).lock(instance);
+            log.debug("attach successful");
         } catch (RuntimeException re) {
             log.error("attach failed", re);
             throw re;
