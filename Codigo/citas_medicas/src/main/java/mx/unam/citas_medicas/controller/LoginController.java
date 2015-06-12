0a331e225914c96d439b001410e8cef01313b0b7
silverdraccus <silver.draccus@gmail.com>
2015-06-11 22:21:57 -0500

    help!

--- Codigo/citas_medicas/src/main/java/mx/unam/citas_medicas/controller/LoginController.java
+++ Codigo/citas_medicas/src/main/java/mx/unam/citas_medicas/controller/LoginController.java
@@ -1,11 +1,22 @@
 package mx.unam.citas_medicas.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 /**
  *
  * @author Ariadna
  */
+@Controller
+@RequestMapping("/LoginController")
 public class LoginController {
-    
+    @RequestMapping(method = RequestMethod.POST)
+    public String printHello(ModelMap model) {
+        System.out.println("aaaaa");
+        return "X";
+    }
 }
