7446f8cc6feb669adfc6c524adf98796ed618421
ariadnaModebo <ariadna@modebo.com>
2015-06-16 23:44:20 -0500

    configuracion funcional

--- Codigo/citas_medicas/src/main/java/mx/unam/citas_medicas/controller/LoginController.java
+++ Codigo/citas_medicas/src/main/java/mx/unam/citas_medicas/controller/LoginController.java
@@ -1,14 +1,10 @@
 package mx.unam.citas_medicas.controller;
 
-import java.net.URL;
-import java.net.URLClassLoader;
 import mx.unam.citas_medicas.modelo.Usuario;
 import mx.unam.citas_medicas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -37,7 +33,6 @@ public class LoginController {
     
     //Para loguear
     @RequestMapping(value= "/login.jsp",  method=RequestMethod.POST)
-    //@ModelAttribute("usuario") 
     public String getLogin(Usuario u){
         
         u=usuarioService.getUsuarioByNombreAndByPassword(u);
